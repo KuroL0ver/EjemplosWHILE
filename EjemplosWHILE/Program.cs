cf44811b0a3352634d50918bfd185660abef5ef5
Camila Fimbres <camifimbres@gmail.com>
2024-10-21 07:45:20 -0700

    Agregar archivos de proyecto.

---
+++ EjemplosWHILE/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace EjemplosWHILE
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int repetir = 100;
+            int i = 0; //Este es un contador
+
+            while (i < repetir)
+            {
+                i ++;
+                Console.WriteLine("Bienvenido  i=" + i);
+            }
+
+
+        }
+    }
+}
+       
+       
